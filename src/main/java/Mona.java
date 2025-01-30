9fbd59c488db86de4ea49f625fe4bbe9145204e1
Wilkins Ang <angweijian2001@gmail.com>
2025-01-30 15:27:09 +0800

    Improve the skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Mona.java
+++ src/main/java/Mona.java
@@ -1,6 +1,32 @@
-public class Mona{
+import java.util.Scanner;
+
+public class Mona {
+    private static final String INIT = "What's up, Joker? What are we going to do today?";
+    private static final String NEXT_LINE = "---------------------------------------------------------------------";
+    private static final String GOODBYE = "We should get ready for tomorrow. Goodnight, Joker.";
+
+    public static void echo(String message) {
+        System.out.println(message);
+        System.out.println(NEXT_LINE);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Mona.\nWhat can I do for you?\nBye. Hope to see you soon\n");
+        System.out.println(INIT);
+        System.out.println(NEXT_LINE);
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            String message = input.nextLine();
+            if (message.equalsIgnoreCase("bye")) {
+                break;
+            }
+            echo(message);
+        }
+
+        input.close();
+
+        System.out.println(GOODBYE);
+        System.out.println(NEXT_LINE);
     }
 }
