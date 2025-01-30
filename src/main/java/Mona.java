37b7e086c0e86cef750fa916b9f66f001007f908
Wilkins Ang <angweijian2001@gmail.com>
2025-01-30 15:49:19 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Mona.java
+++ src/main/java/Mona.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mona {
@@ -5,28 +6,50 @@ public class Mona {
     private static final String NEXT_LINE = "---------------------------------------------------------------------";
     private static final String GOODBYE = "We should get ready for tomorrow. Goodnight, Joker.";
 
-    public static void echo(String message) {
-        System.out.println(message);
-        System.out.println(NEXT_LINE);
-    }
+    private static ArrayList<String> list = new ArrayList<String>(100);
 
     public static void main(String[] args) {
-        System.out.println(INIT);
-        System.out.println(NEXT_LINE);
-
         Scanner input = new Scanner(System.in);
 
+        greet();
         while (true) {
             String message = input.nextLine();
             if (message.equalsIgnoreCase("bye")) {
                 break;
             }
-            echo(message);
+            if (message.equalsIgnoreCase("list")) {
+                printList();
+            } else {
+                addList(message);
+            }
         }
 
         input.close();
 
+        bye();
+    }
+
+    public static void greet() {
+        System.out.println(INIT);
+        System.out.println(NEXT_LINE);
+    }
+
+    public static void bye() {
         System.out.println(GOODBYE);
+        System.out.println(NEXT_LINE);
+    }
+
+    public static void addList(String input) {
+        list.add(input);
+        System.out.printf("Okie Joker, I'll help you remember to %s.", input);
+        System.out.println(NEXT_LINE);
+    }
+
+    public static void printList() {
+        System.out.println("Alright Joker, here is what you need to do:");
+        for (int i = 1; i <= list.size(); i++) {
+            System.out.println(i + ": " + list.get(i - 1));
+        }
         System.out.println(NEXT_LINE);
     }
 }
