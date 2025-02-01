609199135555c10314a5aca114efba2c60ea0c6b
Wilkins Ang <angweijian2001@gmail.com>
2025-02-01 22:43:12 +0800

    Add the ability to mark tasks as done and not done

--- src/main/java/Mona.java
+++ src/main/java/Mona.java
@@ -4,28 +4,34 @@ import java.util.Scanner;
 public class Mona {
     private static final String INIT = "What's up, Joker? What are we going to do today?";
     private static final String NEXT_LINE = "---------------------------------------------------------------------";
-    private static final String GOODBYE = "We should get ready for tomorrow. Goodnight, Joker.";
+    private static final String GOODBYE = "We should get ready for tomorrow. Goodnight, Joker. Meowww.";
 
-    private static ArrayList<String> list = new ArrayList<String>(100);
+    private static ArrayList<Task> tasks = new ArrayList<>(100);
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         greet();
-        while (true) {
-            String message = input.nextLine();
-            if (message.equalsIgnoreCase("bye")) {
-                break;
-            }
-            if (message.equalsIgnoreCase("list")) {
+        String message = input.nextLine().toLowerCase();
+
+        while (!message.equals("bye")) {
+            if (message.equals("list")) {
                 printList();
+            } else if (message.contains("mark") || message.contains("unmark")) {
+                String[] intr = message.split(" ");
+                int index = Integer.parseInt(intr[1]) - 1;
+                if (intr[0].equals("mark")){
+                    markTaskDone(tasks.get(index));
+                } else {
+                    markTaskUndone(tasks.get(index));
+                }
             } else {
                 addList(message);
             }
+            System.out.println(NEXT_LINE);
+            message = input.nextLine().toLowerCase();
         }
 
         input.close();
-
         bye();
     }
 
@@ -40,16 +46,39 @@ public class Mona {
     }
 
     public static void addList(String input) {
-        list.add(input);
-        System.out.printf("Okie Joker, I'll help you remember to %s.", input);
-        System.out.println(NEXT_LINE);
+        Task task = new Task(input);
+        tasks.add(task);
+        System.out.printf("Okie Joker, I'll help you remember to %s.\n", input);
     }
 
     public static void printList() {
         System.out.println("Alright Joker, here is what you need to do:");
-        for (int i = 1; i <= list.size(); i++) {
-            System.out.println(i + ": " + list.get(i - 1));
+        if (tasks.isEmpty()) {
+            System.out.println("Waittt, you didn't tell me anything!!");
+        } else {
+            for (int i = 1; i <= tasks.size(); i++) {
+                System.out.println(i + ": " + tasks.get(i - 1));
+            }
         }
-        System.out.println(NEXT_LINE);
+    }
+
+    public static void markTaskDone(Task task) {
+        if (!task.isDone) {
+            task.markAsDone();
+            System.out.println("All right, Joker! Very smooth!");
+        } else {
+            System.out.println("Hey! It's already done!!");
+        }
+        System.out.println(task);
+    }
+
+    public static void markTaskUndone(Task task) {
+        if (task.isDone){
+            task.markAsUndone();
+            System.out.println("What?! Come on, Joker...!");
+        } else {
+            System.out.println("Hey! Did you forget or something?!");
+        }
+        System.out.println(task);
     }
 }
