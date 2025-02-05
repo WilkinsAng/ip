a68e307fbbb515e9758b698d0eb2b364ab86fc69
Wilkins Ang <angweijian2001@gmail.com>
2025-02-05 22:08:57 +0800

    Move Enums for better OOP

--- src/main/java/Mona.java
+++ src/main/java/Mona.java
@@ -1,32 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-enum Command {
-    TODO,
-    DEADLINE,
-    EVENT,
-    DELETE,
-    MARK,
-    UNMARK,
-    LIST,
-    BYE;
 
-    public static Command fromString(String command) throws MonaException {
-        try {
-            return Command.valueOf(command.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new MonaException.UnknownCommandException(command);
-        }
-    }
-
-    public static String allCommands() {
-        StringBuilder commands = new StringBuilder();
-        for (Command command : Command.values()) {
-            commands.append("\n").append(command.name());
-        }
-        return commands.toString();
-    }
-}
 public class Mona {
     private static final String INIT = "What's up, Joker? What are we going to do today?";
     private static final String NEXT_LINE = "---------------------------------------------------------------------";
